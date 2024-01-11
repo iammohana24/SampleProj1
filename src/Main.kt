270cd5faac4c436b0e1a45af6865f3f96b9775f8
Mohana Krishna <v-mshivuni@microsoft.com>
2024-01-11 00:17:35 -0500

    First Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() 
+fun main()
 {
-    println("Hello Start Program")
+    val name = "Kotlin"
+    val name1 = "program"
+    println(name)
 
-    
 }
